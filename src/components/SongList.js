3714ac741b14e4b61ca60f8c3379515d3794255c
sori <sori.coralcat@developer.com>
2025-08-21 21:05:43 +0900

    컴포넌트 내에서 API 연동, .gitignore 설정 추가

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,30 +1,64 @@
-export function SongList() {
+export async function SongList() {
+  const response = await fetch(
+    "http://itunes.apple.com/search?term=coldplay&entity=song&limit=20"
+  );
+  const data = await response.json();
+
+  const songs = data.results.map((item, idx) => ({
+    artist: item.artistName,
+    title: item.trackName,
+    artwork: item.artworkUrl60,
+    index: idx + 1,
+    genre: item.primaryGenreName,
+    preview: item.previewUrl,
+  }));
+
+  const rows = songs
+    .map(
+      (item) => `
+      <ul>
+        <li title="check">
+          <div class="checkbox">
+            <input id="check-${item.index}" type="checkbox" />
+            <label for="check-${item.index}"></label>
+          </div>
+        </li>
+        <li title="title">
+          <img
+            src="${item.artwork}"
+            alt=""
+          />
+          <div>${item.title}</div>
+        </li>
+        <li>${item.artist}</li>
+        <li>${item.genre}</li>
+        <li>
+          <audio
+            controls=""
+            src="${item.preview}"
+          ></audio>
+        </li>
+      </ul>
+    `
+    )
+    .join("");
+
   return ` 
     <section>
       <div class="list">
-        <ul>
+        <ul class="title">
           <li title="check">
             <div class="checkbox">
-              <input id="check" type="checkbox" />
-              <label for="check"></label>
+              <input id="checkAll" type="checkbox" />
+              <label for="checkAll"></label>
             </div>
           </li>
-          <li title="title">
-            <img
-              src="https://is1-ssl.mzstatic.com/image/thumb/Features125/v4/60/90/ad/6090adc3-8863-861d-afcc-23c55c6fe5da/dj.vmtulfyu.jpg/60x60bb.jpg"
-              alt=""
-            />
-            <div>A Sky Full of Stars</div>
-          </li>
-          <li>Coldplay</li>
-          <li>Alternative</li>
-          <li>
-            <audio
-              controls=""
-              src="https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview116/v4/a2/31/4b/a2314b97-10b6-190c-72b3-45cc21bbf56b/mzaf_740612971315603868.plus.aac.p.m4a"
-            ></audio>
-          </li>
+          <li title="title">title</li>
+          <li>artist</li>
+          <li>genre</li>
+          <li>preview</li>
         </ul>
+        ${rows}
       </div>
     </section>
   `;
--- src/main.js
+++ src/main.js
@@ -6,11 +6,37 @@ import { Header } from "@/components/Header.js";
 import { Overview } from "@/components/Overview.js";
 import { SongList } from "@/components/SongList.js";
 
-document.querySelector("#app").innerHTML = `
+(async () => {
+  document.querySelector("#app").innerHTML = `
   ${Aside()}
   <main class="content-music">
     ${Header()}
     ${Overview()}
-    ${SongList()}
+    ${await SongList()}
   </main>
 `;
+
+  const checkAll = document.getElementById("checkAll");
+  const checkboxes = document.querySelectorAll("input[type='checkbox']");
+
+  checkAll.addEventListener("click", (event) => {
+    checkboxes.forEach((checkbox) => {
+      checkbox.checked = event.target.checked;
+    });
+  });
+
+  checkboxes.forEach((checkbox) => {
+    checkbox.addEventListener("click", () => {
+      const allChecked = [...checkboxes]
+        .filter((checkbox) => checkbox !== checkAll)
+        .every((checkbox) => checkbox.checked);
+      checkAll.checked = allChecked;
+    });
+  });
+
+  const menu = document.querySelector("header .menu");
+  menu.addEventListener("click", () => {
+    const sidebar = document.querySelector(".sidebar");
+    sidebar.classList.toggle("is-active");
+  });
+})();
